f2f32608c24ef900af3bebb264b229a92f09780d
Pawl Otrebski <pkotrebski2@gmail.com>
2017-07-05 22:11:38 +0200

    modified the first source

--- src/index.php
+++ src/index.php
@@ -1,4 +1,26 @@
 <?php
 
-echo 'loader';
+class Index {
 
+      protected $name;
+
+      protected $surname;
+
+      public function __construct()
+      {
+	$this->name = "";
+	$this->surname = "";
+      }
+
+      public function setName(string $name)
+      {
+	$this->name = $name;
+      }
+
+      public function getName()
+      {
+	return $this->name;
+      }
+      
+
+}
